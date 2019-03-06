b0a48740c58c4cb60f14163625d4cca0cca98e1a
weixinwang <weixinwang@gmail.com>
2019-03-06 17:07:37 -0500

    match the kai function for the two spectral methods

--- Bouncing ball/hybrid2.m
+++ Bouncing ball/hybrid2.m
@@ -50,12 +50,12 @@ y_xfour = shift2.*y_xfour;
 lamda = zeros(n1,n2);
 lamda(x1>=0&x1<0.5,x2<=0) = repmat(1e2/exp(0.1/epsilonLamda)*exp(-x1(x1>=0&x1<0.5)/epsilonLamda),1,length(find(x2<=0)));
 lamda(x1<0,x2<=0) = repmat(1e2,length(find(x1<0)),length(find(x2<=0)));
-kai = zeros(1,n2,n1,n2);
+kai = zeros(n1,n2,n1,n2);
 for m_1 = 1:n1
     for n_1 = 1:n2
-        for m_2 = find(abs(x1+x1(m_1))<1e-3 | [false(n1/2,1);m_1==1;false(n1/2-1,1)])
+        for m_2 = find(abs(x1-abs(x1(m_1)))<1e-3 | [false(n1/2,1);m_1==1;false(n1/2-1,1)])
             for n_2 = 1:n2
-                if x2(n_1) <= 0
+                if x2(n_1) < 0
                     kai(m_1,n_1,m_2,n_2) = n1/L1/(sqrt(2*pi)*sigmaV)*exp(-(x2(n_2)+c*x2(n_1))^2/(2*sigmaV^2));
                 else
                     if n_1 == n_2
