0a7cce8dc2fc82cdc178ba4c93a46430b571c0ba
weixinwang <weixinwang@gmail.com>
2019-03-09 13:12:33 -0500

    Try to match discrete propagation of Spectral with MonteCarlo

--- Bouncing ball/discrete2.m
+++ Bouncing ball/discrete2.m
@@ -1,19 +1,21 @@
-clear; close all;
+function [fx] = discrete2()
+
+close all;
+addpath('tests');
 
 % parameters
-c = 1;                                      % coefficient of restitution
-sigmaC = 0.1;                               % standard deviation of coefficient of restitution
-epsilonLamda = 0.1;                         % concentration parameter for transition rate
-sigmaX1 = 0.5;                              % concentration parameter for position reset
+c = 0.95;                                   % coefficient of restitution
+sigmaV = 0.5;                               % standard deviation of coefficient of restitution
+epsilonLamda = 0.036;                        % concentration parameter for transition rate
 x0 = [0;-3];                                % initial condition
-sigma0 = [0.5^2,0;0,0.5^2];                 % covariance matrix of initial condition
+sigma0 = [0.3^2,0.04;0.04,0.5^2];           % covariance matrix of initial condition
 
 % grid
-n1 = 50; n2 = 50;
-L1 = 20; L2 = 12;
+n1 = 100; n2 = 50;
+L1 = 5; L2 = 16;
 x1 = linspace(-L1/2,L1/2-L1/n1,n1).'; x1(abs(x1)<1e-10) = 0;
 x2 = linspace(-L2/2,L2/2-L2/n2,n2); x2(abs(x2)<1e-10) = 0;
-dt = 0.5;
+dt = 1/40;
 
 % initial density function
 f0 = @(x)1/(2*pi)/sqrt(det(sigma0))*exp(-1/2*(x-x0)'*sigma0^-1*(x-x0));
@@ -33,18 +35,25 @@ y(:,:,1) = shift1.*shift2.*y(:,:,1);
 
 % transition kernal and rate
 lamda = zeros(n1,n2);
-lamda(x1>=0,x2<=0) = repmat(1e2*epsilonLamda*exp(-x1(x1>=0)/epsilonLamda),1,length(find(x2<=0)));
-lamda(x1<0,x2<=0) = repmat(lamda(x1==0,x2==0),length(find(x1<0)),length(find(x2<=0)));
+lamda(x1>=0&x1<0.5,x2<=0) = repmat(5e2/exp(0.1/epsilonLamda)*exp(-x1(x1>=0&x1<0.5)/epsilonLamda),1,length(find(x2<=0)));
+lamda(x1<0,x2<=0) = repmat(5e2,length(find(x1<0)),length(find(x2<=0)));
 kai = zeros(1,n2,n1,n2);
-for n_1 = setdiff(1:n2,find(x2==0))
-    for m_2 = 1:n1
-        for n_2 = 1:n2
-            kai(1,n_1,m_2,n_2) = 1/sqrt(2*pi)/sigmaX1*exp(-x1(m_2)^2/2/sigmaX1^2)/(sqrt(2*pi)*sigmaC*abs(x2(n_1)))*...
-                exp(-(x2(n_2)+c*x2(n_1))^2/(2*sigmaC^2*x2(n_1)^2));
+for m_1 = 1:n1
+    for n_1 = 1:n2
+        for m_2 = find(abs(x1+x1(m_1))<1e-3 | [false(n1/2,1);m_1==1;false(n1/2-1,1)])
+            for n_2 = 1:n2
+                if x2(n_1) <= 0
+                    kai(m_1,n_1,m_2,n_2) = n1/L1/(sqrt(2*pi)*sigmaV)*exp(-(x2(n_2)+c*x2(n_1))^2/(2*sigmaV^2));
+                else
+                    if n_1 == n_2
+                        kai(m_1,n_1,m_2,n_2) = n1/L1*n2/L2;
+                    end
+                end
+            end
         end
+        kai(m_1,n_1,:,:) = kai(m_1,n_1,:,:)/sum(sum(kai(m_1,n_1,:,:)*L1*L2/n1/n2));
     end
 end
-kai = repmat(kai,n1,1);
 
 % fft of transition kernal and rate
 y_lamda = fftshift(fft2(lamda))/n1/n2;
@@ -101,4 +110,8 @@ view([0,0,1]);
 figure;
 surf(x2,x1,fx(:,:,2));
 view([0,0,1]);
+
+rmpath('tests');
+
+end
 
