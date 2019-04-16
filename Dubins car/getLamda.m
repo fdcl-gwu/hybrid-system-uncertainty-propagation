5803d2cdda2f9772ec5cd5d4f15c1b02211e6ba7
weixinwang <weixinwang@gmail.com>
2019-04-16 11:55:32 -0400

    A better lamda

--- Dubins car/getLamda.m
+++ Dubins car/getLamda.m
@@ -4,18 +4,21 @@ function [ lamda ] = getLamda( x1, x2, xo1, xo2 )
 assert(size(x1,2)==1 && size(x2,2)==1, 'x1 and x2 must be column vectors');
 
 % parameters
-epsilonIn = 6;
+epsilonIn = 8;
+epsilonOut = 8;
 cIn = 400;
-epsilonOut = 0.3;
 cOut = 100;
+dOut = 0.5;
 
 % lamda
-lamda = [zeros(size(x1,1),3),cOut*ones(size(x1,1),4)];
+lamda = zeros(size(x1,1),4);
 for no = 1:length(xo1)
     distance = sqrt(sum(([x1,x2]-[xo1(no),xo2(no)]).^2,2));
     lamda(:,no) = cIn*exp(-distance*epsilonIn);
-    lamda(:,4) = lamda(:,4).*exp(-epsilonOut./distance);
+    lamda(:,4) = lamda(:,4)+cOut*exp(-(distance-dOut)*epsilonOut);
 end
+lamda(:,4) = cOut-lamda(:,4);
+lamda(lamda(:,4)<0,4) = 0;
 
 end
 
