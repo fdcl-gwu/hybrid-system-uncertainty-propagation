caf1821d7a7b71b2ec31b94c99350923b8ed3b9b
weixinwang <wwang442@wisc.edu>
2019-08-15 12:58:49 -0400

    parameters for the movie for grant meeting

--- Bouncing ball/getParameter.m
+++ Bouncing ball/getParameter.m
@@ -23,5 +23,28 @@ if model == 1
     p.Lt = 6;
 end
 
+if model == 2
+    % parameters
+    p.g = 9.8;
+    p.niu = 0.1;
+    p.sigma = 0.02;
+    p.c = 0.9;
+    p.sigmaV = 0.3;
+    p.sigmaM = 0.3;
+    p.nSample = 1000000;
+    
+    % initial condition
+    p.x0 = [1.5;0];
+    p.sigma0 = [0.1^2,0;0,0.2^2];
+    
+    % grid
+    p.N1 = 200;
+    p.N2 = 200;
+    p.L1 = 5;
+    p.L2 = 16;
+    p.Nt = 801;
+    p.Lt = 20;
+end
+
 end
 
